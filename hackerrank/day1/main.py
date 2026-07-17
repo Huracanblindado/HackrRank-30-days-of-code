c3caf60c58d0a2e24a827c4d0561fce6e459ac74
Huracanblindado <blindadohuracan@gmail.com>
2026-07-17 07:22:32 +0000

    reto#1

---
+++ hackerrank/day1/main.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+entero = 0
+decimal = 0,0 
+cadena_texto = ''
+
+# Read and save an integer, double, and String to your variables.
+entero = int(input ())
+decimal = float(input ())
+cadena_texto = input ()
+
+# Print the sum of both integer variables on a new line.
+print (i+entero)
+
+# Print the sum of the double variables on a new line.
+print (d+decimal)
+
+# Concatenate and print t9he String variables on a new line
+# The 's' variable above should be printed first.
+print(f'{s}{cadena_texto}')
