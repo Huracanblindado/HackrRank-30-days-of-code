3745b34aef09f9438cc42e6393fc270c0f7c8b0b
Huracanblindado <blindadohuracan@gmail.com>
2026-07-19 18:51:06 +0000

    reto3

---
+++ hackerrank/day2/main.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    print(f" El monto total a pagar en caja es: {round(meal_cost+(meal_cost*(tip_percent/100))+(meal_cost*(tax_percent/100)))}")
+
+
+if __name__ == '__main__':
+    meal_cost = float(input("Digite el monto bruto de la comida(decimal): ").strip())
+
+    tip_percent = int(input("Digite el porcentaje de la propina (entero): ").strip())
+
+    tax_percent = int(input("Digite el porcentaje del impuesto (entero): ").strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
---
+++ hackerrank/day3/main.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+
+'''
+tarea: Dado un numero entero n, realiza las siguientes acciones condicionales:
+1 Si N es impar, imprime Weird.
+2 Si N es par y esta en el rango inclusivo de 2 a 5, imprime Not Weird.
+3 Si N es par y esta en el rango inclusivo de 6 a 20, imprime Weird.
+4 Si N es par y es mayor que 20, imprime Not Weird.
+'''
+
+
+if(N%2  !=  0):
+    print('Weird')
+elif (N%2  == 0 and N >= 2 and N <= 5):
+    print('Not Weird')
+elif (N%2 == 0 and N >= 6 and N <= 20):
+        print('Weird')
+elif (N%2 == 0 and N > 20):
+    print('Not Weird')
